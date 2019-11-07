dc51ced1bb1c6681d589d871979b74d4448a139a
Keith Rose <37423459+KeithAndreRose@users.noreply.github.com>
2019-11-06 21:09:12 -0500

    key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDxg0d3W7jbl5LI6QLFFfRsiv-FD-Dzkdw",
-  authDomain: "crypt-a8c7d.firebaseapp.com",
-  databaseURL: "https://crypt-a8c7d.firebaseio.com",
-  projectId: "crypt-a8c7d",
-  storageBucket: "crypt-a8c7d.appspot.com",
-  messagingSenderId: "894570777285",
-  appId: "1:894570777285:web:4727b2e5c9f80e0b"
-  }
+  firebaseConfig: null
 };
 
 /*
